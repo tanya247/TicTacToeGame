b4d2adbbfd3e3961fb29afa7cd2b618551ae8782
tanya <tanyakansal238@gmail.com>
2021-04-25 23:25:17 +0530

    UC3 showed

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -5,6 +5,9 @@ public class TicTacToeGame {
 		createBoard();
 		char userLetter=chooseLetter();
 		char compLetter=(userLetter =='x')?'o' :'x';
+		char show[] = createBoard();
+		showBoard(show);
+		
 	}	
 	static char[] createBoard(){
 		char Board[] = new char[10];
@@ -31,5 +34,13 @@ public class TicTacToeGame {
 			return chooseLetter();
 		}
 	}
-		
+	static void showBoard( char gameBoard[]){
+		System.out.println(" ");
+	    System.out.println("\n "+ gameBoard[1] + " | "+ gameBoard[2] + " | " +gameBoard[3]);
+	    System.out.println("-------------");
+	    System.out.println(" "+ gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6]);
+	    System.out.println("-------------");
+	    System.out.println(" "+ gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);
+	    System.out.println(" ");
+	}	
 }
