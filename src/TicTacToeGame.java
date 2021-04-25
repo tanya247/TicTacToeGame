8853a32e8a4a6725680ab9ac1761beb35b4a19b3
tanya <tanyakansal238@gmail.com>
2021-04-25 23:15:55 +0530

    UC2 showed

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");
 		createBoard();
+		char userLetter=chooseLetter();
+		char compLetter=(userLetter =='x')?'o' :'x';
 	}	
 	static char[] createBoard(){
 		char Board[] = new char[10];
@@ -11,5 +14,22 @@ public class TicTacToeGame {
 		}
 		return Board;
 	}
-	
+	static char chooseLetter()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose the Letter o or x");
+		char Letter =sc.next().toLowerCase().charAt(0);
+		
+		if(Letter == 'o'){
+			return Letter;
+		}
+		else if(Letter == 'x'){
+			return Letter;
+		}
+		else {
+			System.out.println("Invalid Choice ");
+			return chooseLetter();
+		}
+	}
+		
 }
