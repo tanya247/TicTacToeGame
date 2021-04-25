64a81751d77815a53d486052efd6b365c50cf692
Tanya <82596524+tanya247@users.noreply.github.com>
2021-04-26 00:10:49 +0530

    UC4 showed

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -10,8 +10,8 @@ public class TicTacToeGame {
 		char show[] = createBoard();
 		showBoard(show);
 		int userMove = desireMove(show,scanner);
-	    show[userMove]=userLetter;
-	    showBoard(show);
+	    	show[userMove]=userLetter;
+	    	showBoard(show);
 		
 	}	
 	
@@ -42,22 +42,22 @@ public class TicTacToeGame {
 	}
 	static void showBoard( char gameBoard[]){
 		System.out.println(" ");
-	    System.out.println("\n "+ gameBoard[1] + " | "+ gameBoard[2] + " | " +gameBoard[3]);
-	    System.out.println("-------------");
-	    System.out.println(" "+ gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6]);
-	    System.out.println("-------------");
-	    System.out.println(" "+ gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);
-	    System.out.println(" ");
+	    	System.out.println("\n "+ gameBoard[1] + " | "+ gameBoard[2] + " | " +gameBoard[3]);
+	    	System.out.println("-----------------");
+	    	System.out.println(" "+ gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6]);
+	    	System.out.println("-----------------");
+	    	System.out.println(" "+ gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);
+	    	System.out.println(" ");
 	}	
 	static int desireMove(char board[],Scanner scanner){
-		int validCells[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	    while (true){
-	    	System.out.println("What is your next move ? (1-9)");
-	    	int index = scanner.nextInt();
-	    	if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index)){
-	    		return index;
+		Integer [] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	    	while (true){
+	    		System.out.println("What is your next move ? (1-9)");
+	    		int index = scanner.nextInt();
+	    		if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index)){
+	    			return index;
+	    		}
 	    	}
-	    }
 	    
 	}
 	private static boolean isSpaceFree(char[] board, int index){
